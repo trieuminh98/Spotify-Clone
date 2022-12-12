ec3aae27eff59378ea5e9407ef9be8331e15beb6
trieuminh98 <trieuminh98>
2022-12-12 17:13:20 +0700

    Update

--- src/fragments/home/sider/index.tsx
+++ src/fragments/home/sider/index.tsx
@@ -4,65 +4,16 @@ import { JSXElement } from 'commons/interfaces/common';
 import { Col } from 'components';
 import Menu from 'components/Menu';
 import Row from 'components/Row';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Sider.module.scss';
 
 type SiderProps = {
   children?: JSXElement;
 };
 
-const HomeLogo = () => {
-  return (
-    <svg role='img' height='24' width='24' aria-hidden='true' viewBox='0 0 24 24' data-encore-id='icon'>
-      <path d='M13.5 1.515a3 3 0 00-3 0L3 5.845a2 2 0 00-1 1.732V21a1 1 0 001 1h6a1 1 0 001-1v-6h4v6a1 1 0 001 1h6a1 1 0 001-1V7.577a2 2 0 00-1-1.732l-7.5-4.33z'></path>
-    </svg>
-  );
-};
-
-const SearchLogo = () => {
-  return (
-    <svg role='img' height='24' width='24' aria-hidden='true' viewBox='0 0 24 24' data-encore-id='icon'>
-      <path d='M10.533 1.279c-5.18 0-9.407 4.14-9.407 9.279s4.226 9.279 9.407 9.279c2.234 0 4.29-.77 5.907-2.058l4.353 4.353a1 1 0 101.414-1.414l-4.344-4.344a9.157 9.157 0 002.077-5.816c0-5.14-4.226-9.28-9.407-9.28zm-7.407 9.279c0-4.006 3.302-7.28 7.407-7.28s7.407 3.274 7.407 7.28-3.302 7.279-7.407 7.279-7.407-3.273-7.407-7.28z'></path>
-    </svg>
-  );
-};
-
-const LibraryLogo = () => {
-  return (
-    <svg role='img' height='24' width='24' aria-hidden='true' viewBox='0 0 24 24' data-encore-id='icon'>
-      <path d='M14.5 2.134a1 1 0 011 0l6 3.464a1 1 0 01.5.866V21a1 1 0 01-1 1h-6a1 1 0 01-1-1V3a1 1 0 01.5-.866zM16 4.732V20h4V7.041l-4-2.309zM3 22a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1zm6 0a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1z'></path>
-    </svg>
-  );
-};
-
-const menuData = [
-  {
-    icon: <HomeLogo />,
-    label: 'Home',
-  },
-  {
-    icon: <SearchLogo />,
-    label: 'Search',
-  },
-  {
-    icon: <LibraryLogo />,
-    label: 'Your Library',
-  },
-];
-
-const items2: MenuProps['items'] = menuData.map((menu, index: number) => {
-  const key = String(index + 1);
-
-  return {
-    key: `sub${key}`,
-    icon: menu.icon,
-    label: menu.label,
-  };
-});
-
 const SpotifyLogo = () => {
   return (
-    <svg viewBox='0 0 1134 340' className={`${styles['sider']}`}>
+    <svg viewBox='0 0 1134 340' className={`${styles['logo']}`}>
       <title>Spotify</title>
       <path
         fill='currentColor'
@@ -73,6 +24,71 @@ const SpotifyLogo = () => {
 };
 
 const Sider = (_props: SiderProps) => {
+  const [current, setCurrent] = useState('1');
+
+  const HomeLogo = () => {
+    return (
+      <svg role='img' height='24' width='24' aria-hidden='true' viewBox='0 0 24 24' data-encore-id='icon'>
+        <path
+          fill='#fff'
+          d='M13.5 1.515a3 3 0 00-3 0L3 5.845a2 2 0 00-1 1.732V21a1 1 0 001 1h6a1 1 0 001-1v-6h4v6a1 1 0 001 1h6a1 1 0 001-1V7.577a2 2 0 00-1-1.732l-7.5-4.33z'
+        ></path>
+      </svg>
+    );
+  };
+
+  const SearchLogo = () => {
+    return (
+      <svg role='img' height='24' width='24' aria-hidden='true' viewBox='0 0 24 24' data-encore-id='icon'>
+        <path d='M15.356 10.558c0 2.623-2.16 4.75-4.823 4.75-2.664 0-4.824-2.127-4.824-4.75s2.16-4.75 4.824-4.75c2.664 0 4.823 2.127 4.823 4.75z'></path>
+        <path
+          fill='#fff'
+          d='M1.126 10.558c0-5.14 4.226-9.28 9.407-9.28 5.18 0 9.407 4.14 9.407 9.28a9.157 9.157 0 01-2.077 5.816l4.344 4.344a1 1 0 01-1.414 1.414l-4.353-4.353a9.454 9.454 0 01-5.907 2.058c-5.18 0-9.407-4.14-9.407-9.28zm9.407-7.28c-4.105 0-7.407 3.274-7.407 7.28s3.302 7.279 7.407 7.279 7.407-3.273 7.407-7.28c0-4.005-3.302-7.278-7.407-7.278z'
+        ></path>
+      </svg>
+    );
+  };
+
+  const LibraryLogo = () => {
+    return (
+      <svg role='img' height='24' width='24' aria-hidden='true' viewBox='0 0 24 24' data-encore-id='icon'>
+        <path
+          fill='#fff'
+          d='M14.5 2.134a1 1 0 011 0l6 3.464a1 1 0 01.5.866V21a1 1 0 01-1 1h-6a1 1 0 01-1-1V3a1 1 0 01.5-.866zM16 4.732V20h4V7.041l-4-2.309zM3 22a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1zm6 0a1 1 0 01-1-1V3a1 1 0 012 0v18a1 1 0 01-1 1z'
+        ></path>
+      </svg>
+    );
+  };
+
+  const menuData = [
+    {
+      icon: <HomeLogo />,
+      label: 'Home',
+    },
+    {
+      icon: <SearchLogo />,
+      label: 'Search',
+    },
+    {
+      icon: <LibraryLogo />,
+      label: 'Your Library',
+    },
+  ];
+
+  const items2: MenuProps['items'] = menuData.map((menu, index: number) => {
+    const key = String(index + 1);
+
+    return {
+      key: `sub${key}`,
+      icon: menu.icon,
+      label: menu.label,
+    };
+  });
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    setCurrent(e.key);
+  };
+
   return (
     <Row className='bg-black'>
       <Col mT24 span={24}>
@@ -82,11 +98,12 @@ const Sider = (_props: SiderProps) => {
       </Col>
       <Col className='w-full h-screen pt-5'>
         <Menu
-          className='bg-black'
+          onClick={onClick}
+          className={`bg-black ${styles['menu']}`}
           mode='inline'
-          defaultSelectedKeys={['1']}
+          selectedKeys={[current]}
           defaultOpenKeys={['sub1']}
-          style={{ height: '100%', borderRight: 0 }}
+          style={{ height: '100%', borderRight: 0, color: '#fff' }}
           items={items2}
         />
       </Col>
